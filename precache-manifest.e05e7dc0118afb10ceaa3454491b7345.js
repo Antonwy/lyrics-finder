e04bb02eebb1e68fe5ffeb73e0fc5054331569b0
Anton Wyrowski <anton.wyrowski@gmail.com>
2019-01-12 13:34:34 +0100

    Updates

---
+++ precache-manifest.e05e7dc0118afb10ceaa3454491b7345.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e4bbd3c3f241158cbe70",
+    "url": "/lyrics-finder/static/js/runtime~main.e4bbd3c3.js"
+  },
+  {
+    "revision": "774e69284cb1ac42d131",
+    "url": "/lyrics-finder/static/js/main.774e6928.chunk.js"
+  },
+  {
+    "revision": "7c49302a866889c9692d",
+    "url": "/lyrics-finder/static/js/1.7c49302a.chunk.js"
+  },
+  {
+    "revision": "774e69284cb1ac42d131",
+    "url": "/lyrics-finder/static/css/main.f508f553.chunk.css"
+  },
+  {
+    "revision": "7ffe7aaf5d01d968c6b3729417f1b986",
+    "url": "/lyrics-finder/index.html"
+  }
+];
