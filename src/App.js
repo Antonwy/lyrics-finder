09014cee69c4306adea3d959aebdd05f7f7605a5
Anton Wyrowski <anton.wyrowski@gmail.com>
2019-01-12 13:32:26 +0100

    first commit!

--- src/App.js
+++ src/App.js
@@ -1,25 +1,81 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Axios from 'axios';
+
+const url = "https://api.lyrics.ovh/v1/"
 
 class App extends Component {
+
+  state = {
+    artist: "",
+    title: "",
+    lyrics: "",
+    name: "",
+    error: {
+      code: 0,
+      text: ""
+    }
+  }
+
+  // errorCodes:
+  //   0: default value
+  //   1: no input
+  //   2: wrong input
+
+  onSubmitText = (input) => {
+    input.preventDefault();
+    const {artist, title} = this.state;
+    
+    if(artist === "" || title === "" || artist === "" && title === ""){
+      this.setState({error: {text: "Type in some values!", code: 1}})
+      return
+    }
+
+    Axios.get(`${url}${artist}/${title}`)
+      .then(res => {
+        this.setState({
+          lyrics: res.data.lyrics,
+          name: this.getName(),
+          error: {text: "", code: 0}
+        });
+      })
+      .catch(err => {
+        this.setState({
+          error: {text: "Can't find this song!", code: 2}
+        })
+      })
+  }
+
+  onChangeInput = (type) => (event) => {
+    
+    if(this.state.error.code === 1){
+      this.setState({error: {text: "", code: 0}})
+    }
+    
+    this.setState({
+      [type]: event.target.value
+    })
+  }
+
+  getName = () => {
+    const {artist, title} = this.state;
+    const formattedArtist = artist.toUpperCase();
+    const formattedTitle = title.toUpperCase();
+    return formattedArtist + " - " + formattedTitle;
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <h1 className="title">Lyrics Finder</h1>
+        <form className="searchForm" onSubmit={this.onSubmitText}>
+          <input name="artist" value={this.state.artist} onChange={this.onChangeInput('artist')} type="text" placeholder="Artist"></input>
+          <input name="title" value={this.state.title} onChange={this.onChangeInput('title')} type="text" placeholder="Title"></input>
+          <button type="submit">Search</button>
+          <p>{this.state.error.text}</p>
+        </form>
+        <h2 className="name">{this.state.name}</h2>
+        <p className="lyrics">{`${this.state.lyrics}`}</p>
       </div>
     );
   }
